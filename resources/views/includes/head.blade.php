e5c3878ad1219181c236f8ad1abc39ddc3e6a11f
Joona Yoon <joonas.yoon@gmail.com>
2015-12-20 11:42:34 +0000

    Add Mathjax

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -20,3 +20,13 @@
     <script src="//oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+    
+    <script type="text/x-mathjax-config">
+      MathJax.Hub.Config({
+        showProcessingMessages: false,
+        tex2jax: {
+          inlineMath: [['$','$'], ['\\(','\\)']]
+        }
+      });
+    </script>
+    <script src="http://cdn.mathjax.org/mathjax/2.4-latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script> 
