39308898571a151f03151f56cb9b2cbcc0e8c8a5
Joona Yoon <joonas.yoon@gmail.com>
2015-12-20 10:54:55 +0000

    Added NanumBarunGothic font

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" type="text/css" href="/assets/semantic-ui/css/semantic.min.css" />
     <link rel="stylesheet" type="text/css" href="/assets/style.css" />
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="/fonts/nanumbarungothic.css" />
 
     <script type="text/javascript" src="/assets/semantic-ui/jquery/jquery-2.1.4.min.js"></script>
     <script type="text/javascript" src="/assets/semantic-ui/css/semantic.min.js"></script>
