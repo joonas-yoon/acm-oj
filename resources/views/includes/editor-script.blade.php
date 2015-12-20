0096f4738da0eec346b48a620d752a9ff35afe99
Joona Yoon <joonas.yoon@gmail.com>
2015-12-20 22:47:58 +0000

    Fix bug on editor

--- resources/views/includes/editor-script.blade.php
+++ resources/views/includes/editor-script.blade.php
@@ -9,20 +9,23 @@
       menu: {},
       menubar: false,
       toolbar: 
-        'undo redo | bold italic underline strikethrough \
+        'styleselect | undo redo | bold italic underline strikethrough \
         | subscript superscript | alignleft aligncenter alignright \
         | bullist numlist | codesample visualblocks table \
         | link image fontawesome | preview code fullscreen'
       ,
       plugins : [
-        'advlist autolink link image lists table preview visualblocks code codesample',
-        'fontawesome fullscreen'
+        'advlist autolink link image lists table preview visualblocks paste',
+        'code codesample autoresize fontawesome fullscreen'
       ],
       relative_urls: false,
       force_br_newlines : true,
-      force_p_newlines : false,
-      forced_root_block : false,
+      force_p_newlines : true,
+      forced_root_block : '',
       remove_linebreaks : true,
+      paste_as_text: true,
+      autoresize_bottom_margin: 50,
+      autoresize_max_height: 700,
     
       height: 300,
       content_css: [
@@ -35,6 +38,18 @@
       table_default_attributes: {
         class: 'ui celled table'
       },
+      
+      style_formats: [
+        { title: "Headers",
+          items: [
+            {title: "Header 1", format: "h2", class: 'ui header'},
+            {title: "Header 2", format: "h3", class: 'ui header'},
+            {title: "Header 3", format: "h4", class: 'ui header'},
+            {title: "Header 4", format: "h5", class: 'ui header'},
+            {title: "Header 5", format: "h6", class: 'ui header'}]
+        },
+        { title: '인용글', block: 'blockquote' },
+      ],
     });
     tinymce.init({
       selector: 'textarea.html-editor-simple',
