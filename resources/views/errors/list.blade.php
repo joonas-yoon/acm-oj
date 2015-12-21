cb8f93bee045173aa47f8c21e85c796cbda29928
Joona Yoon <joonas.yoon@gmail.com>
2015-12-21 11:06:18 +0000

    Bug fix

--- resources/views/errors/list.blade.php
+++ resources/views/errors/list.blade.php
@@ -2,7 +2,7 @@
     <div class="ui warning message">
         <i class="close icon"></i>
         <div class="header">
-            Sorry, please correct them below.
+            앗! 문제가 생겼습니다.
         </div>
         <ul class="ui list">
         @foreach($errors->all() as $error)
